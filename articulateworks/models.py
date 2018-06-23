f3e10ba3c92082fa7ea0872778f13868c15d0e8d
jason <jasonkrue@gmail.com>
2018-06-23 16:34:50 -0700

    Created many to many join models between requesters/applicants and skills/tasks/roles

--- articulateworks/models.py
+++ articulateworks/models.py
@@ -1,34 +1,76 @@
 # Create your models here.
-from django.db.models import CharField, ManyToManyField, ForeignKey, CASCADE
+from django.contrib.auth.models import User
+from django.db.models import CharField, ManyToManyField, ForeignKey, CASCADE, OneToOneField
+from django.db.models.signals import post_save
 from model_utils.models import TimeStampedModel
 
 
 class Requester(TimeStampedModel):
     name = CharField(max_length=255)
-    description = CharField(max_length=1024)
-    admins = ManyToManyField('auth.User')
+    description = CharField(max_length=1024, blank=True)
+    admins = ManyToManyField('auth.User', related_name='requesters')
 
 
-class AdminProfile(TimeStampedModel):
-    user = ForeignKey('auth.User', on_delete=CASCADE)
-    requester = ForeignKey('Requester', on_delete=CASCADE)
+class Profile(TimeStampedModel):
+    user = OneToOneField('auth.User', on_delete=CASCADE, related_name='profile')
+
+    def add_skills(self, skills):
+        if not isinstance(skills, list):
+            skills = (skills,)
+        for skill in skills:
+            if not isinstance(skill, Skill):
+                skill, created = Skill.objects.get_or_create(name=skill)
+            self.user.skills.get_or_create(skill=skill)
+
+
+class RequesterSkill(TimeStampedModel):
+    requester = ForeignKey('Requester', on_delete=CASCADE, related_name='skills')
+    skill = ForeignKey('Skill', on_delete=CASCADE, related_name='requesters')
+
+
+class ApplicantSkill(TimeStampedModel):
+    user = ForeignKey('auth.User', on_delete=CASCADE, related_name='skills')
+    skill = ForeignKey('Skill', on_delete=CASCADE, related_name='applicants')
 
 
 class Skill(TimeStampedModel):
     name = CharField(max_length=255)
-    description = CharField(max_length=1024)
+    description = CharField(max_length=1024, blank=True)
+
+
+class RequesterTask(TimeStampedModel):
+    requester = ForeignKey('Requester', on_delete=CASCADE, related_name='tasks')
+    task = ForeignKey('Task', on_delete=CASCADE, related_name='requesters')
+
+
+class ApplicantTask(TimeStampedModel):
+    user = ForeignKey('auth.User', on_delete=CASCADE, related_name='tasks')
+    task = ForeignKey('Task', on_delete=CASCADE, related_name='applicants')
 
 
 class Task(TimeStampedModel):
     name = CharField(max_length=255)
-    description = CharField(max_length=1024)
+    description = CharField(max_length=1024, blank=True)
+
+
+class RequesterRole(TimeStampedModel):
+    requester = ForeignKey('Requester', on_delete=CASCADE, related_name='roles')
+    role = ForeignKey('Role', on_delete=CASCADE, related_name='requesters')
+
+
+class ApplicantRole(TimeStampedModel):
+    user = ForeignKey('auth.User', on_delete=CASCADE, related_name='roles')
+    role = ForeignKey('Role', on_delete=CASCADE, related_name='applicants')
 
 
 class Role(TimeStampedModel):
     name = CharField(max_length=255)
-    description = CharField(max_length=1024)
+    description = CharField(max_length=1024, blank=True)
 
 
-class Profile(TimeStampedModel):
-    name = CharField(max_length=255)
-    description = CharField(max_length=1024)
+def save_or_create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        profile = Profile.objects.create(user=instance)
+        profile.save()
+
+post_save.connect(save_or_create_user_profile, sender=User)
--- articulateworks/tests.py
+++ articulateworks/tests.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 # Create your tests here.
+from articulateworks.models import Skill
+
+
+class ArticulateTests(TestCase):
+    def setUp(self):
+        # stuff to run before each test_* method
+        pass
+
+    def test_basic(self):
+        user = User.objects.create_user('janedoe')
+        skill = Skill.objects.create(name='French', description='French language')
+        user.profile.add_skills(skill)
+        self.assertEqual(user.skills.first().skill.name, "French")
