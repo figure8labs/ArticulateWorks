e91627e87c24af8effed959f48f91aa0b1177105
Alexis Vabre <info@figure8labs.com>
2018-06-23 15:55:24 -0700

    more skeleton views and comment descriptions

--- articulateworks/views.py
+++ articulateworks/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .forms import ApplicationEntryForm
 from .models import Application
 
+# applicant perspective
+
+# a user can fill out an application in response to a requester
 def create_application(request):
     if request.method == 'POST':
         application = Application()
@@ -10,9 +13,42 @@ def create_application(request):
         form = ApplicationEntryForm
     return render(request, 'articulateworks/new_application.html', {'form': form})
 
-def get_tasks(request):
+# a user can see the list of tasks needed from a requester
+def get_tasks_needed(request):
     return render(request, 'articulateworks/tasks.html')
 
-def get_skills(request):
+# a user can see the list of skills needed from a requester
+def get_skills_needed(request):
     return render(request, 'articulateworks/skills.html')
+
+# a user can see the full list of needs from a requester
+def get_full_needs(request):
+    return render(request, 'articulateworks/needs.html')
+
+# a user can see a list of all jobs based on search criteria from remote server
+def get_jobs(request):
+    return render(request, 'articulateworks/jobs.html')
+
+# a user can see a list of jobs they've been sent personally
+def get_received_jobs(request):
+    return render(request, 'articulateworks/jobs.html')
+
+# obviously this will need to be a blackbox api call once we set up a server
+def get_full_database(request):
+    if request.user.is_subscriber:
+        jobs = get_jobs()
+    else:
+        jobs = get_received_jobs()
+    return render(request, 'articulateworks/jobs.html')
+
+# admin perspective
+
+# a requester can see the profile of who has applied to their requests
+def get_applicants(request):
+    return render(request, 'articulateworks/applicants.html')
+
+# a requester can see the response to their request
+def get_applications(request):
+    return render(request, 'articulateworks/applications.html')
+
 
